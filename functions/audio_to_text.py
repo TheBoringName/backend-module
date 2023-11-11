4e5efa687b04f59586be4845f2061bd1716f0c02
Pevth <236639@edu.p.lodz.pl>
2023-11-11 10:56:35 +0100

    Add multiprocessing to convert audio to text

--- functions/audio_to_text.py
+++ functions/audio_to_text.py
@@ -6,6 +6,9 @@ import random
 from pydub import AudioSegment
 from dotenv import load_dotenv
 
+import multiprocessing
+from queue import Empty
+
 #------------------------------------#
 #             AZURE CREDS            #
 #------------------------------------#
@@ -23,16 +26,17 @@ service_region = os.getenv("AZURE_REGION")
 def transcribe_chunks_to_text(audio_chunk):
     speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=service_region)
     audio_config = speechsdk.audio.AudioConfig(filename=audio_chunk)
-
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
-
     result = speech_recognizer.recognize_once()
-
     if result.reason == speechsdk.ResultReason.RecognizedSpeech:
         return result.text
-    
+
+def process_audio_chunk(chunk_info, result_queue, id):
+    audio_chunk_filename, text_type = chunk_info
+    text = transcribe_chunks_to_text(audio_chunk_filename)
+    result_queue.put((audio_chunk_filename, text, text_type, id))
+
 def split_audio_to_chunks(audio_details):
-    temp_wav_name = "/tmp/" + str(uuid.uuid4()) + ".wav"
 
     audio = AudioSegment.from_file(audio_details['audio_name'])
     audio_details['text'] = ""
@@ -40,46 +44,61 @@ def split_audio_to_chunks(audio_details):
     audio_details['last_words'] = ""
     audio_details['middle_words'] = ""
 
-    chunk_length = 20000
-    ten_minutes = 600000
+    chunk_length = 15000
+    seven_minutes = 450000
+    one_minute = 60000
 
-    num_moments = len(audio) // ten_minutes
+    num_moments = len(audio) // seven_minutes
     moments = []
-    moments_idx = 1
 
     if num_moments > 0:
         for _ in range(num_moments):
-            random_chunk = random.randint(ten_minutes // chunk_length, (len(audio) // chunk_length)-3) * chunk_length
+            random_chunk = random.randint(one_minute // chunk_length, (len(audio) // chunk_length)-4) * chunk_length
             moments.append(random_chunk)
+
+
+    result_queue = multiprocessing.Queue()
+    processes = []
 
     for i in range(0, len(audio), chunk_length):
 
+        if not os.path.exists("/tmp/" + audio_details["title"].replace(" ", "")):
+            os.makedirs("/tmp/" + audio_details["title"].replace(" ", ""))
+
+        temp_wav_name = "/tmp/" + audio_details["title"].replace(" ", "") + "/" + str(uuid.uuid4()) + ".wav"
         audio_chunk = audio[i:i + chunk_length]
-        audio_chunk_filename = temp_wav_name
-        audio_chunk.export(audio_chunk_filename, format="wav")
+        audio_chunk.export(temp_wav_name, format="wav")
 
-
-        if (i + chunk_length <= 40000) or (len(audio) - (i + chunk_length) <= 40000) or i in moments or i - 20000 in moments:
-            time.sleep(5)
-        
-        if i + chunk_length <= 60000:
-            text = transcribe_chunks_to_text(audio_chunk_filename)
-            audio_details['text'] += f" {text}"
-            audio_details['first_words'] += f" {text}"
+        if (i + chunk_length <= 60000):
+            process = multiprocessing.Process(target=process_audio_chunk, args=((temp_wav_name, "first_words"), result_queue, i + chunk_length))
+            process.start()
+            processes.append(process)
 
         elif len(audio) - (i + chunk_length) <= 60000:
-            text = transcribe_chunks_to_text(audio_chunk_filename)
-            audio_details['text'] += f" {text}"
-            audio_details['last_words'] += f" {text}"
+            process = multiprocessing.Process(target=process_audio_chunk, args=((temp_wav_name, "last_words"), result_queue, i + chunk_length))
+            process.start()
+            processes.append(process)
 
         elif i in moments or i-20000 in moments:
-            text = transcribe_chunks_to_text(audio_chunk_filename)
-            audio_details['text'] += f" {text}"
-            audio_details['middle_words'] += f"Zdanie {moments_idx}:  '{text}'"
-            moments_idx += 1
+            process = multiprocessing.Process(target=process_audio_chunk, args=((temp_wav_name, "middle_words"), result_queue, i + chunk_length))
+            process.start()
+            processes.append(process)
 
-#        else:
-#            audio_details['text'] += f" {transcribe_chunks_to_text(audio_chunk_filename)}"
+    for process in processes:
+        process.join()
+
+    try:
+        results = []
+        while True:
+            audio_chunk_filename, text, type_text, type_id = result_queue.get_nowait()
+            results.append((type_id, audio_chunk_filename,type_text, text))
+
+    except Empty:
+        pass
+
+    results.sort(key=lambda x: x[0])
+
+    for result in results:
+        audio_details[result[2]] += f" {result[3]}"
 
     return audio_details
-
