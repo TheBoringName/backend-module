88f89991f7e17f0e086515c2e2ee2c097edc3837
Pevth <236639@edu.p.lodz.pl>
2023-12-04 21:06:00 +0100

    add float type

--- functions/sentiment.py
+++ functions/sentiment.py
@@ -11,15 +11,15 @@ def sentiment_analyze_via_azure(audio_details):
 
     text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
 
-    positive = 0
-    neutral = 0
-    negative = 0
+    positive = 0.0
+    neutral = 0.0
+    negative = 0.0
 
     if "text" in audio_details:
         result = text_analytics_client.analyze_sentiment(audio_details["text"], show_opinion_mining=True)
-        positive = result.confidence_scores.positive
-        neutral = result.confidence_scores.neutral
-        negative = result.confidence_scores.negative
+        positive = float(result.confidence_scores.positive)
+        neutral = float(result.confidence_scores.neutral)
+        negative = float(result.confidence_scores.negative)
 
     else:
         all_text = [audio_details["first_words"], audio_details["middle_words"],  audio_details["last_words"]]
@@ -27,18 +27,18 @@ def sentiment_analyze_via_azure(audio_details):
         texts = [text for text in result if not text.is_error]
 
         for text in enumerate(texts):
-            positive += text.confidence_scores.positive
-            neutral += text.confidence_scores.neutral
-            negative += text.confidence_scores.negative
+            positive += float(text.confidence_scores.positive)
+            neutral += float(text.confidence_scores.neutral)
+            negative += float(text.confidence_scores.negative)
 
 
 
     if positive > neutral and positive > negative:
         return "positive"
-    elif neutral > positive and neutral > negative:
-        return "neutral"
-    else:
+    elif negative > positive and negative > neutral:
         return "negative"
+    else:
+        return "neutral"
 
 
 
